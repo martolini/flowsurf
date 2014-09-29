0d82fa9d1be0e6e734b8db063b306c274877e130
Martin Skow Røed <msroed@gmail.com>
2014-09-29 14:53:43 +0200

    added prod settings

--- flowsurf/settings.py
+++ flowsurf/settings.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import socket
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+PRODUCTION = 'cauchy' in socket.gethostname()
 
 
 # Quick-start development settings - unsuitable for production
@@ -64,6 +67,10 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+if PRODUCTION:
+    STATIC_ROOT = '/opt/flowenv/static'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
